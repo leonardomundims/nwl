257630326c7f76c286a17c38a2ed5e916dd51e98
Leonardo Mundim <leo.mundimsouza@gmail.com>
2020-06-04 21:48:33 -0300

    add nodeJs ao projeto, com express e nunjucks

---
+++ src/server.js
@@ -0,0 +1,42 @@
+//dependencias usadas
+//express para o server
+const express = require("express")
+const server = express()
+
+//template engine -> transforma html estático em dinâmico
+const nunjucks = require("nunjucks")
+    //configurar o nunjucks  
+nunjucks.configure("src/views", //pasta onde estão os arq. html
+    {
+        express: server, //quem é o servidor express
+        noCache: true //desabilita o uso de memória cache (true em desenvolvimento, false em prod)
+    })
+
+
+// configurar pasta publica/public
+server.use(express.static("public"))
+
+
+//configurar as rotas do servidor
+//pagina inicial home = "/"
+//req: Requisiçao
+//resp: Resposta
+server.get("/", (req, resp) => {
+    return resp.render("index.html") //resposta a requisição de acesso a pagina inicial
+})
+
+//pagina create-point
+server.get("/create-point", (req, resp) => {
+    return resp.render("create-point.html")
+})
+
+//pagina search-results
+server.get("/search-results", (req, resp) => {
+    return resp.render("search-results.html")
+})
+
+
+// ligar o servidor na porta 3000
+server.listen(3000)
+    // depois no terminal dar comando node e passar o caminho do arquivo server.js, nesse caso
+    // node src/server.js -> da para criar um atalho para essa função no arquivo package.json
